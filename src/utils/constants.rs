f392b0643c950a28bc34766e34d73075cecfe9f1
0xLucca <0xlucca.dev@gmail.com>
2023-03-03 11:43:52 -0300

    Add features check

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,14 +1,25 @@
+pub const ALLOWED_FEATURES: [&str; 6] = [
+    "psp22",
+    "psp34",
+    "psp37",
+    "pausable",
+    "ownable",
+    "access-control",
+];
+
 pub const CARGO_TOML: &str = "[package]\n\
 name = \"my_psp22\"\n\
 version = \"1.0.0\"\n\
 edition = \"2021\"\n\
 authors = [\"The best developer ever\"]\n\
+\n\
 [dependencies]\n\
 ink = { version = \"~4.0.0\", default-features = false }\n\
 scale = { package = \"parity-scale-codec\", version = \"3\", default-features = false, features = [\"derive\"] }\n\
 scale-info = { version = \"2.3\", default-features = false, features = [\"derive\"], optional = true }\n\
 # Include brush as a dependency and enable default implementation for PSP22 via brush feature\n\
 openbrush = { tag = \"3.0.0\", git = \"https://github.com/727-Ventures/openbrush-contracts\", default-features = false, features = [features_list] }\n\
+\n\
 [lib]\n\
 name = \"my_psp22\"\n\
 path = \"lib.rs\"\n\
@@ -16,6 +27,7 @@ crate-type = [\n\
     # Used for normal contract Wasm blobs.\n\
     \"cdylib\",\n\
 ]\n\
+\n\
 [features]\n\
 default = [\"std\"]\n\
 std = [\n\
--- src/utils/contract_utils.rs
+++ src/utils/contract_utils.rs
@@ -1,31 +1,31 @@
-use crate::utils::constants::CARGO_TOML; // Maybe we can use directly from module
+use crate::utils::constants::{ALLOWED_FEATURES, CARGO_TOML}; // Maybe we can use directly from module
 use std::fs::File;
 use std::io::prelude::*;
 
-pub fn create_cargo_toml_file(features: &Vec<String>) {
-    // TODO Define a tmp path for this file
-    let mut cargo_toml_file = File::create("./src/Cargo.toml").unwrap();
+pub fn create_cargo_toml_file(features: &Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
+    // Check if sent features are allowed
+    for feature in features {
+        if !ALLOWED_FEATURES.contains(&feature.as_str()) {
+            return Err("Feature not allowed".into());
+        }
+    }
 
-    //TODO Check if features exist
     let features_list = features
         .iter()
         .map(|s| format!("\"{}\"", s))
         .collect::<Vec<String>>()
         .join(", ");
 
-    println!("features_list: {}", features_list);
-
     let cargo_toml_file_contents = CARGO_TOML.replace("features_list", &features_list);
 
-    cargo_toml_file
-        .write_all(cargo_toml_file_contents.as_bytes())
-        .unwrap();
+    // TODO Define a tmp path for this file
+    let mut cargo_toml_file = File::create("./src/Cargo.toml")?;
+    cargo_toml_file.write_all(cargo_toml_file_contents.as_bytes())?;
+
+    Ok(())
 }
 
-pub fn create_lib_rs_file(code: String) {
-    // let mut lib_rs_file = File::create("src/lib.rs").unwrap();
-    // let mut lib_rs_file_contents = format!(LIB_RS, code = code);
-    // lib_rs_file
-    //     .write_all(lib_rs_file_contents.as_bytes())
-    //     .unwrap();
+pub fn create_lib_rs_file(code: &String) {
+    let mut lib_rs_file = File::create("src/lib.rs").unwrap();
+    lib_rs_file.write_all(code.as_bytes()).unwrap();
 }
