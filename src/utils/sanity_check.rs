66c7084a12cf6154a102c06ede87e42198f23e7b
Gabriel Camba Gonzalez <gabrielnicolasgonzalez@gmail.com>
2023-03-24 15:44:18 -0300

    adding tests

--- src/utils/sanity_check.rs
+++ src/utils/sanity_check.rs
@@ -131,4 +131,97 @@ mod sanity_check_tests {
         let error = &result.err().unwrap();
         assert_eq!(error.0, Status::InternalServerError);
     }
+
+    #[test]
+    fn test_sanity_check_on_empty_features() {
+        let wizard_message = WizardMessage {
+            code: String::from_utf8(vec![b'1'; MAX_SIZE_ALLOWED - 1]).unwrap(),
+            address: String::from(BOB),
+            features: vec![],
+        };
+
+        let expected_error = Err(Custom(
+            Status::InternalServerError,
+            Json(ServerResponse::new_error(String::from(
+                "Features must not be empty.",
+            ))),
+        ));
+
+        let result = sanity_check(&Json(wizard_message));
+        assert_eq!(result, expected_error);
+        assert_eq!(result.is_err(), true);
+        let error = &result.err().unwrap();
+        assert_eq!(error.0, Status::InternalServerError);
+    }
+
+    #[test]
+    fn test_sanity_check_on_not_valid_features() {
+        let wizard_message = WizardMessage {
+            code: String::from_utf8(vec![b'1'; MAX_SIZE_ALLOWED - 1]).unwrap(),
+            address: String::from(BOB),
+            features: vec![
+                String::from("psp22"),
+                String::from("pausable"),
+                String::from("recoverable"),
+            ],
+        };
+
+        let expected_error = Err(Custom(
+            Status::InternalServerError,
+            Json(ServerResponse::new_error(String::from(
+                "Feature not allowed",
+            ))),
+        ));
+        let result = sanity_check(&Json(wizard_message));
+        assert_eq!(result, expected_error);
+        assert_eq!(result.is_err(), true);
+        let error = &result.err().unwrap();
+        assert_eq!(error.0, Status::InternalServerError);
+    }
+
+    #[test]
+    fn test_sanity_check_on_ambiguous_standard() {
+        let wizard_message = WizardMessage {
+            code: String::from_utf8(vec![b'1'; MAX_SIZE_ALLOWED - 1]).unwrap(),
+            address: String::from(BOB),
+            features: vec![
+                String::from("psp22"),
+                String::from("pausable"),
+                String::from("psp34"),
+            ],
+        };
+
+        let expected_error = Err(Custom(
+            Status::InternalServerError,
+            Json(ServerResponse::new_error(String::from(
+                "Feature contains ambiguous contract standard",
+            ))),
+        ));
+        let result = sanity_check(&Json(wizard_message));
+        assert_eq!(result, expected_error);
+        assert_eq!(result.is_err(), true);
+        let error = &result.err().unwrap();
+        assert_eq!(error.0, Status::InternalServerError);
+    }
+
+    #[test]
+    fn test_sanity_check_on_no_standard() {
+        let wizard_message = WizardMessage {
+            code: String::from_utf8(vec![b'1'; MAX_SIZE_ALLOWED - 1]).unwrap(),
+            address: String::from(BOB),
+            features: vec![String::from("pausable")],
+        };
+
+        let expected_error = Err(Custom(
+            Status::InternalServerError,
+            Json(ServerResponse::new_error(String::from(
+                "Features must contain at least one contract standard",
+            ))),
+        ));
+        let result = sanity_check(&Json(wizard_message));
+        assert_eq!(result, expected_error);
+        assert_eq!(result.is_err(), true);
+        let error = &result.err().unwrap();
+        assert_eq!(error.0, Status::InternalServerError);
+    }
 }
