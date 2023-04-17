63a4221183e1d9062ba7a2a99b708745609650df
0xLucca <0xlucca.dev@gmail.com>
2023-04-17 13:11:54 -0300

    feat: extract CORS from main to file

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,6 @@ use api::contract_api::{
 };
 use repository::mongodb_repo::MongoRepo;
 use rocket::fairing::AdHoc;
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
-use rocket::{Request, Response};
 use std::{
     sync::{atomic::AtomicBool, Arc},
     thread,
@@ -27,27 +24,7 @@ use utils::compiler::Compiler;
 use log::{debug, info};
 use log4rs;
 
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to responses",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new(
-            "Access-Control-Allow-Methods",
-            "POST, GET, PATCH, OPTIONS",
-        ));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
-}
+use utils::cors::CORS;
 
 // Rocket launching the server in rocket function
 #[launch]
